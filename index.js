92173b162c9218ca65ab1169b41f5681bca64c3f
Kauã Wikleff <139168172+DevKakau@users.noreply.github.com>
2024-09-10 10:31:49 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {select, input} = require('@inquirer/prompts')
+const {select, input, checkbox} = require('@inquirer/prompts')
 
 let meta = {
     value: "Beber 3l de água",
@@ -14,8 +14,38 @@ const cadastrarMeta = async() =>{
         console.log('Não pode cadastrar uma meta vazia.')
         return;
         // se eu utilizar somente o return, vai encerrar a função e voltar para o menu, mas se eu quiser posso deixar o usuario preso até ele escrever uma meta basta utilizar o return cadastrarMeta();
+    } else if(meta == " "){
+        console.log('Não pode cadastrar uma meta vazia');
+        return
     }
     metas.push( {value: meta, checked: false }); // adicionando meta ao array; 
+}
+
+const listarMetas = async() => {
+    const respostas = await checkbox({
+        message: 'use as setas para mudar de meta, o espaço para marcar e desmarcar e o Enter para finalizar essa etapa.',
+        choices: [...metas],
+        instructions: false, // criando uma copia do array metas
+    })
+
+    metas.forEach((m) =>{
+        m.checked = false
+    })
+
+    if(respostas.length == 0 ){
+        console.log('Nenhuma meta selecionada!')
+        return
+    }
+
+    respostas.forEach((resposta) => {
+        const meta  =  metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked  = true;
+    })
+
+    console.log('Meta(s) concluídas(s)')
 }
 
 const start = async () => {
@@ -49,7 +79,7 @@ const start = async () => {
                 console.log(metas);
                 break
             case "listar":
-                console.log("Vamos Listar");
+                await listarMetas();
                 break
             case "sair":
                 console.log("Encerrando programa.");
